f0b35631cf161e352ced02eca00d757d5658f828
Xerkerr <berkersaatci96@gmail.com>
2023-05-07 14:52:57 +0300

    v4

--- src/test/java/day05_relativeLocators_maven/C02_MavenIlkClass.java
+++ src/test/java/day05_relativeLocators_maven/C02_MavenIlkClass.java
@@ -3,25 +3,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-//1. http://zero.webappsecurity.com sayfasina gidin
-//2. Signin buttonuna tiklayin
-//3. Login alanine  “username” yazdirin
-//4. Password alanina “password” yazdirin
-//5. Sign in buttonuna tiklayin
-//6. Back tusu ile sayfaya donun
-//7. Online Banking menusunden Pay Bills sayfasina gidin
-//8. amount kismina yatirmak istediginiz herhangi bir miktari yazin
-//9. tarih kismina “2020-09-10” yazdirin
-//10. Pay buttonuna tiklayin
-//11. “The payment was successfully submitted.” mesajinin ciktigini test edin
+
 public class C02_MavenIlkClass {
     public static void main(String[] args) {
+
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-//1
-        driver.get("http://zero.webappsecurity.com");
+        driver.get("https://www.amazon.com");
 
+        driver.close();
     }
 }
---
+++ src/test/java/day05_relativeLocators_maven/C03_ZeroWebapp.java
@@ -0,0 +1,32 @@
+package day05_relativeLocators_maven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_ZeroWebapp {//1. http://zero.webappsecurity.com sayfasina gidin
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+//1. http://zero.webappsecurity.com sayfasina gidin
+        driver.get("http://zero.webappsecurity.com");
+//2. Signin buttonuna tiklayin
+
+//3. Login alanine  “username” yazdirin
+//4. Password alanina “password” yazdirin
+//5. Sign in buttonuna tiklayin
+//6. Back tusu ile sayfaya donun
+//7. Online Banking menusunden Pay Bills sayfasina gidin
+//8. amount kismina yatirmak istediginiz herhangi bir miktari yazin
+//9. tarih kismina “2020-09-10” yazdirin
+//10. Pay buttonuna tiklayin
+//11. “The payment was successfully submitted.” mesajinin ciktigini test edin
+    }
+
+}
