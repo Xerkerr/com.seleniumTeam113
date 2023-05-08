0cdbe045aac31625e43fe481781199d26a029441
Xerkerr <berkersaatci96@gmail.com>
2023-05-08 18:32:44 +0300

    v5

--- src/test/java/day05_relativeLocators_maven/C03_ZeroWebapp.java
+++ src/test/java/day05_relativeLocators_maven/C03_ZeroWebapp.java
@@ -1,7 +1,9 @@
 package day05_relativeLocators_maven;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -17,9 +19,12 @@ public class C03_ZeroWebapp {//1. http://zero.webappsecurity.com sayfasina gidin
 //1. http://zero.webappsecurity.com sayfasina gidin
         driver.get("http://zero.webappsecurity.com");
 //2. Signin buttonuna tiklayin
-
+         driver.findElement(By.id("signin_button")).click();
 //3. Login alanine  “username” yazdirin
+        WebElement loginKutusu = driver.findElement(By.id("user_login"));
+        loginKutusu.sendKeys("username");
 //4. Password alanina “password” yazdirin
+ 
 //5. Sign in buttonuna tiklayin
 //6. Back tusu ile sayfaya donun
 //7. Online Banking menusunden Pay Bills sayfasina gidin
